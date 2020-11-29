009e0c57a138b71265f29bbe70f307a53501d78d
Mobassir <mobassirhossen94@gmail.com>
2020-11-29 19:40:54 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ Created on thursday september 24 04:39:33 2020
 from flask import Flask, render_template, url_for, request
 import pandas as pd
 import Levenshtein as lev
+import re
 
 app = Flask(__name__)
 
@@ -16,48 +17,57 @@ def home():
 
 @app.route('/predict',methods=['GET','POST'])
 def predict():
-        fullbanglaBanglish = pd.read_csv('Full_Bangla-Banglish.csv')
-        fullbanglaBanglish = fullbanglaBanglish.drop(13094)
-        benn = []
-        zamanbhaiBanglish = "banglish_zamanBhaiCorpus.txt"
-        text_file = open(zamanbhaiBanglish, "r")
-        lines = text_file.readlines()
-        for i  in range(len(lines)):
-                data = lines[i].rstrip("\n")
-                data = data.split('\t')
-                benn.append(data[0])
-        text_file.close()
 
-        fullbanglaBanglish = fullbanglaBanglish.banglish.tolist()
-        fullbanglaBanglish = [x for x in fullbanglaBanglish if str(x) != 'nan']
-        fullbanglaBanglish = " ".join(fullbanglaBanglish)
-        benn = " ".join(benn)
-        banglishDictionary = fullbanglaBanglish + benn
-        
+    fullbanglaBanglish = pd.read_csv('Full_Bangla-Banglish.csv')
+    fullbanglaBanglish = fullbanglaBanglish.drop(13094)
+    benn = []
+    romanicBanglaLyrics = "RomanicBanglaSongsLyrics.txt"
+    
+    #https://stackoverflow.com/questions/42339876/error-unicodedecodeerror-utf-8-codec-cant-decode-byte-0xff-in-position-0-in
 
+    #with open(path , "rb") as f: # encoding = 'utf-8'
+    with open(romanicBanglaLyrics, encoding="utf8", errors='ignore') as f:
+        text = f.read()
+    
+    text2 = re.sub(r'[^a-z ]+', '', text.lower().replace('\n', ' '))
 
-        if request.method == 'POST':
-                comment = request.form['comment']
-                data = comment #[comment]
-
-                def getMatch(string1,string2):
-                    min_sim = .80
-                    output = []
-                    res = [[lev.jaro_winkler(x,y) for x in string1.split()] for y in string2.split()]
-                    print(res)
-                    for x in res:
-                        if max(x) >= min_sim:
-                            output.append(string1.split()[x.index(max(x))])
-                    return output
-                prediction = getMatch(banglishDictionary,data)
-                prediction = " ".join(prediction)
-               
+    for i  in range(len(text2)):
+        data = text2[i].rstrip("\n")
+        data = data.split('\t')
+        benn.append(data[0])
 
 
-        return render_template('result.html', prediction=prediction)
+
+    fullbanglaBanglish = fullbanglaBanglish.banglish.tolist()
+    fullbanglaBanglish = [x for x in fullbanglaBanglish if str(x) != 'nan']
+    fullbanglaBanglish = " ".join(fullbanglaBanglish)
+    benn = " ".join(benn)
+    banglishDictionary = fullbanglaBanglish + benn
+    
+
+
+    if request.method == 'POST':
+            comment = request.form['comment']
+            data = comment #[comment]
+
+            def getMatch(string1,string2):
+                min_sim = .90
+                output = []
+                res = [[lev.jaro_winkler(x,y) for x in string1.split()] for y in string2.split()]
+
+                for x in res:
+                    if max(x) >= min_sim:
+                        output.append(string1.split()[x.index(max(x))])
+                return output
+            prediction = getMatch(banglishDictionary,data)
+            prediction = " ".join(prediction)
+           
+
+
+    return render_template('result.html', prediction=prediction)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=33507)
+    app.run(debug=1,use_reloader=0, port=33507) #use_reloader=0,
 
 
